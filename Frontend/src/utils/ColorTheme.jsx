eb9c597eb345d85e0347d48ac0d3dfbec22d7fe7
Kelechi Emmanuel <ekelechi413@gmail.com>
2026-08-15 14:30:30 +0100

    done

--- Frontend/src/utils/ColorTheme.jsx
+++ Frontend/src/utils/ColorTheme.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { FiSun } from "react-icons/fi";
-import { FaCircleHalfStroke } from "react-icons/fa6";
+import { RiMoonFill } from "react-icons/ri";
 
 const ColorTheme = () => {
     const [dark, setDark] = useState(false)
@@ -10,7 +10,7 @@ const ColorTheme = () => {
     }, [dark])
     return (
         <button onClick={() => setDark(!dark)} className='text-green-700'>
-            {dark ? <FiSun size={24} /> : <FaCircleHalfStroke size={24} />}
+            {dark ? <FiSun size={24} /> : <RiMoonFill size={24} />}
         </button>
     )
 }
